4a96c6bb4b5dfcaad2cbcedf1786eafd5315a08c
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-11-27 08:18:44 +0200

    Exemple

---
+++ if-else.temperatures.py
@@ -0,0 +1,13 @@
+# METEO source < air temperature
+#                Celsius
+
+# OUT: determin if its "Warm" , "Cold"
+
+
+temperatureC = 10
+
+
+if temperatureC >= 18:
+    print("Warm")
+else:
+    print("Cold")
